01bf6cd0369a4d4744111be1ff9178eb2f78c012
mothibimalebana <mothibi.mm12@gmail.com>
2025-03-16 11:56:01 +0200

    installed react-router-dom

---
+++ src/assets/components/navbar/Navbuttons.jsx
@@ -0,0 +1,11 @@
+const NavButton = ({label}) =>{
+    return(
+        <>
+            <div className="nav-btn">
+                <button>{label}</button>
+            </div>
+        </>
+    )
+}
+
+export default NavButton
