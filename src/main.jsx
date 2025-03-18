b94a56ff3d92cc0fc50989d9e97b81a7df996582
mothibimalebana <mothibi.mm12@gmail.com>
2025-03-18 19:04:12 +0200

    created Cart.jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
