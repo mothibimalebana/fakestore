90d9ff68252ed4f9c6ce089fc930fa3aa8de1c29
mothibimalebana <mothibi.mm12@gmail.com>
2025-03-22 23:09:36 +0200

    quantity has an intital value set by state

--- src/assets/components/content/Product.jsx
+++ src/assets/components/content/Product.jsx
@@ -1,14 +1,23 @@
 import { useOutletContext, useParams } from "react-router-dom"
 import styles from '../../css/product.module.css';
+import { useId, useState } from "react";
 
 
 const Product = () => {
+    const [quntity, setQuantity] = useState(0)
     const { id } = useParams();
     const [ products ] = useOutletContext();
     const product = products[id-1];
+    const quantityInput = useId();
+
+    const handleSubmit = () => {
+
+    }
+
     if(!products[id-1]){
         return <p>{`Page does not exist`}</p>
     }
+
 
 
     return(
@@ -23,12 +32,17 @@ const Product = () => {
                         <div className={styles.title}><h2>{product.title}</h2></div>
                         <div className={styles.price}>{product.price}</div>
                         <div className="description purchase">{product.description}</div>
-                        <div className={styles.quantity}>
-                            <div className="decr"><button className={styles.qButtons}>-</button></div>
-                            <input className={styles.qInput} type="number" name="quantity" id="" />
-                            <div className="incr"><button className={styles.qButtons}>+</button></div>
+                        <div className={styles.buttons}>
+                            <div className={styles.quantity}>
+                                <div className="decr"><button onClick={handleSubmit} className={styles.qButtons}>-</button></div>
+                                <label htmlFor={quantityInput}></label>
+                                <input className={styles.qInput} value={quntity} onChange={handleSubmit} name="quantity" id={quantityInput} />
+                                <div className="incr"><button className={styles.qButtons}>+</button></div>
+                            </div>
+                            <div className="addToCart">
+                                <button onClick={handleSubmit} className={styles.cButton}>Add to cart</button>
+                            </div>
                         </div>
-                        <div className="addToCart"></div>
                     </div>
                 </div> 
                 )
